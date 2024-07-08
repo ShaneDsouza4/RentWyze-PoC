10b652402643fe5acd6972b12be24a0ceefc4862
ShaneDsouza4 <shane.json96@gmail.com>
2024-07-08 17:27:40 -0400

    Get All Users, Single User, Update User, Verify User, DeleteUser

--- controllers/user.js
+++ controllers/user.js
@@ -43,7 +43,7 @@ async function handleUserLogin(req, res){
 
         res.status(200).json({msg:"success"});
     }catch(error){
-        console.log(error)
+        //console.log(error)
         return res.status(500).json({msg:"User Login Error", error});
     }  
 }
@@ -53,9 +53,69 @@ async function handleUserLogout(req, res){
     res.status(200).json({msg:"Logout Success"});
 }
 
+async function handlegetUserByID(req, res){
+    try{
+        const user = await User.findById(req.params.id);
+        if(!user){
+            return res.status(404).json({msg:"No user found!"});
+        }
+        return res.status(200).res.json({msg:"success", user});
+    }catch(error){
+        return res.status(500).json({msg:"error getting user", error});
+    }
+}
+
+async function handleupdateUserByID(req, res) {
+    try{
+        const result = await User.findByIdAndUpdate(req.params.id, req.body);
+        return res.status(200).json({msg:"success"});
+    }catch(error){
+        return res.status(500).json({msg:"error updating user", error});
+    }
+    
+}
+
+async function handleDeleteUserByID(req, res) {
+    try{
+        await User.findByIdAndDelete(req.params.id);
+        return res.status(200).json({msg:"Delete Success!"});
+    }catch(error){
+        return res.status(500).json({msg:"error deleting user", error});
+    }
+}
+
+async function handleVerifyUserByID(req, res) {
+    try{
+        const { id } = req.params;
+        const { verified } = req.body;
+
+        await User.findByIdAndDelete(
+            { _id: id },
+            { verified },
+        );
+        return res.status(200).json({msg:"Verification Success!"});
+    }catch(error){
+        return res.status(500).json({msg:"error verifying user", error});
+    }
+}
+
+async function handleGetAllUsers(req, res) {
+    try{
+        const allDBUsers = await User.find({});
+        return res.status(200).json(allDBUsers);
+    }catch(error){
+        return res.status(500).json({msg:"error geting all users", error});
+    }
+    
+}
 
 module.exports = {
     handleUserSignup,
     handleUserLogin,
-    handleUserLogout
+    handleUserLogout,
+    handlegetUserByID,
+    handleupdateUserByID,
+    handleDeleteUserByID,
+    handleVerifyUserByID,
+    handleGetAllUsers
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,28 @@
 const express = require("express");
-const { handleUserSignup, handleUserLogin, handleUserLogout } = require("../controllers/user")
+const { 
+    handleUserSignup, 
+    handleUserLogin, 
+    handleUserLogout, 
+    handlegetUserByID,
+    handleupdateUserByID ,
+    handleDeleteUserByID,
+    handleVerifyUserByID,
+    handleGetAllUsers
+} = require("../controllers/user")
 const router = express.Router();
 
 router.post("/signup", handleUserSignup);
 router.post("/login", handleUserLogin);
-router.get("/logout", handleUserLogout)
+router.get("/logout", handleUserLogout); 
+
+
+router.get('/', handleGetAllUsers);
+
+router.route("/:id")
+    .get(handlegetUserByID)
+    .patch(handleupdateUserByID)
+    .delete(handleDeleteUserByID)
+
+router.patch("/verify/:id", handleVerifyUserByID);
 
 module.exports = router;
