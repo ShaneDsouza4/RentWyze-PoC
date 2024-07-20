eb377fb8ed72d7da40cfcd27b41c31f0a6aeb6af
ShaneDsouza4 <shane.json96@gmail.com>
2024-07-20 16:14:53 -0400

    Properties and Reviews Model

--- models/property.js
+++ models/property.js
@@ -1,14 +1,56 @@
 const { Schema, model } = require("mongoose");
 
 const propertySchema = new Schema({
-    name: { type: String, required: true },
-    address: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    postalCode: { type: String, required: true },
-    landlord: { type: mongoose.Schema.Types.ObjectId, ref: 'Landlord'},
-    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
-    averageRating: { type: Number, default: 0 },
+    name: { 
+        type: String, 
+        required: true 
+    },
+    propertyType: {
+        type: String,
+        enum: ["House", "Apartment", "Basement"],
+        required: true
+    },
+    address: { 
+        street:{
+            type: String, 
+            required: true 
+        },
+        city: { 
+            type: String, 
+            required: true 
+        },
+        state: { 
+            type: String, 
+            required: true 
+        },
+        postalCode: { 
+            type: String, 
+            required: true 
+        }
+    },
+    bedrooms: {
+        type: Number,
+        required: true
+    },
+    bathrooms: {
+        type: Number,
+        required: true
+    },
+    squareFeet: {
+        type: Number
+    },
+    amenities: [{
+        type: String
+    }],
+    landlord: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'user'
+    }
+}, { timestamps: true });
+
+propertySchema.pre('save', function(next) {
+    this.updated_at = Date.now();
+    next();
 });
 
 const Property = model('property', propertySchema);
---
+++ models/reviews.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    property_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'property',
+        required: true
+    },
+    tenant_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    review: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+reviewSchema.pre('save', function(next) {
+    this.updated_at = Date.now();
+    next();
+});
+
+const Review = model('review', reviewSchema);
+
+module.exports = Review;
